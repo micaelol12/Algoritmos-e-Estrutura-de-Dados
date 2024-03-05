06b27f1b0691d37d1e9712a9a45ed8b33e428bb4
Micael Luan Conti <mlconti@furb.br>
2024-03-05 19:21:28 -0300

    format code

--- src/exercicio1/ListaEstatica.java
+++ src/exercicio1/ListaEstatica.java
@@ -1,109 +1,115 @@
 package exercicio1;
 
 public class ListaEstatica {
-	
-private int[] info;
-	
-private int tamanho;
-	
- public ListaEstatica() {
-	setInfo(new int[10]);
-	setTamanho(0);
-}
- 
- private void redimensionar() {
-	 int novoTamanho = getTamanho() + 10;
-	 int[] novoVetor = new int[novoTamanho];
-	 for(int i = 0;i  <= getTamanho()-1;i++) {
-		 novoVetor[i] = getInfo()[i];
-	 }
-	 setInfo(novoVetor);
- }
- 
- public void inserir(int valor) {
-	 if(getTamanho() == getInfo().length) {
-		 redimensionar();
-	 }
-	 getInfo()[getTamanho()] = valor;
-	 setTamanho(getTamanho() + 1);;
- }
- 
- public void exibir() {
-	 
- }
- 
- public int buscar(int valor) {
-	for(int i =0; i <= getTamanho() -1;i++) {
-		int aux = getInfo()[i];
+
+	private int[] info;
+
+	private int tamanho;
+
+	public ListaEstatica() {
+		setInfo(new int[10]);
+		setTamanho(0);
+	}
+
+	private void redimensionar() {
+		int novoTamanho = getTamanho() + 10;
+		int[] novoVetor = new int[novoTamanho];
+
+		for (int i = 0; i < getTamanho(); i++) {
+			novoVetor[i] = getInfo()[i];
+		}
 		
-		if(aux == valor) {
-			return i;
+		setInfo(novoVetor);
+	}
+
+	public void inserir(int valor) {
+		if (getTamanho() == getInfo().length) {
+			redimensionar();
+		}
+		getInfo()[getTamanho()] = valor;
+		setTamanho(getTamanho() + 1);
+	}
+
+	public void exibir() {
+		for (int i = 0; i < getTamanho(); i++) {
+			System.out.println(getInfo()[i]);
 		}
 	}
-	return -1;
- }
- 
- public void retirar(int valor) {
-	 int posicao = buscar(valor);
-	 
-	 if(posicao == -1) {
-		 return;
-	 }
-	 
-	 for(int i = posicao + 1;i <= getTamanho() -1;i++) {
-		 int aux = getInfo()[i];
-		 getInfo()[i -1] = aux;
-	 }
-	 
-	 
-	 setTamanho(getTamanho() -1);;
- }
- 
- public void liberar() {
-	 int[] novoVetor = new int[10];
-	 setTamanho(0);
-	 setInfo(novoVetor);
- }
- 
- public int obterElemento(int posicao) {
-	if(posicao > getTamanho()){
-		throw new IndexOutOfBoundsException("Índice " + posicao + " fora dos limites para comprimento " + getTamanho() );
+
+	public int buscar(int valor) {
+		for (int i = 0; i < getTamanho(); i++) {
+			int aux = getInfo()[i];
+
+			if (aux == valor) {
+				return i;
+			}
+		}
+		return -1;
 	}
-	 int valor = getInfo()[posicao];
-	 return valor;
- }
- 
- public boolean estaVazia() {
-	 return getTamanho() == 0;
- }
- 
- public String toString() {
-	 String valor = "";
-	 
-		for(int i =0; i <= getTamanho() -1;i++) {
+
+	public void retirar(int valor) {
+		int posicao = buscar(valor);
+
+		if (posicao == -1) {
+			return;
+		}
+
+		for (int i = posicao + 1; i < getTamanho(); i++) {
+			int aux = getInfo()[i];
+			getInfo()[i - 1] = aux;
+		}
+
+		setTamanho(getTamanho() - 1);
+	}
+
+	public void liberar() {
+		int[] novoVetor = new int[10];
+		setTamanho(0);
+		setInfo(novoVetor);
+	}
+
+	public int obterElemento(int posicao) throws IndexOutOfBoundsException {
+		if (posicao > getTamanho()) {
+			throw new IndexOutOfBoundsException(
+					"Índice " + posicao + " fora dos limites para comprimento " + getTamanho());
+		}
+		int valor = getInfo()[posicao];
+		return valor;
+	}
+
+	public boolean estaVazia() {
+		return getTamanho() == 0;
+	}
+
+	public String toString() {
+		String valor = "";
+
+		for (int i = 0; i < getTamanho(); i++) {
 			valor += getInfo()[i];
-			
-			if(i != getTamanho() -1) {
+
+			if (i != getTamanho() - 1) {
 				valor += ",";
 			}
-			
+
 		}
-		
+
 		return valor;
- }
- 
-public int[] getInfo() {
-	return info;
-}
-public void setInfo(int[] info) {
-	this.info = info;
-}
-public int getTamanho() {
-	return tamanho;
-}
-public void setTamanho(int tamanho) {
-	this.tamanho = tamanho;
-}
- 
- 
+	}
+
+	public int[] getInfo() {
+		return info;
+	}
+
+	public void setInfo(int[] info) {
+		this.info = info;
+	}
+
+	public int getTamanho() {
+		return tamanho;
+	}
+
+	public void setTamanho(int tamanho) {
+		this.tamanho = tamanho;
+	}
+
 }
--- src/exercicio1/ListaEstativaTest.java
+++ src/exercicio1/ListaEstativaTest.java
@@ -7,58 +7,58 @@ import org.junit.jupiter.api.Test;
 
 class ListaEstativaTest {
 	ListaEstatica teste = new ListaEstatica();
-	
-	   @BeforeEach
-	    public void setUp() {
-		   	teste.liberar();
-			teste.inserir(5);
-			teste.inserir(10);
-			teste.inserir(15);
-			teste.inserir(20);
-			
-	    }
-	
+
+	@BeforeEach
+	public void setUp() {
+		teste.liberar();
+		teste.inserir(5);
+		teste.inserir(10);
+		teste.inserir(15);
+		teste.inserir(20);
+
+	}
+
 	@Test
 	void test1() {
 		String retornoE = "5,10,15,20";
-		
+
 		String retorno = teste.toString();
-		
+
 		assertEquals(retorno, retornoE);
 	}
-	
+
 	@Test
 	void test2() {
-		
+
 		int tamanho = teste.getTamanho();
-		
+
 		assertEquals(tamanho, 4);
 	}
-	
+
 	@Test
 	void test3() {
 		assertEquals(teste.buscar(15), 2);
 	}
-	
+
 	@Test
 	void test4() {
 		assertEquals(teste.buscar(30), -1);
 	}
-	
+
 	@Test
 	void test5() {
 		teste.retirar(10);
-		
+
 		String retornoE = "5,15,20";
-		
+
 		String retorno = teste.toString();
 		assertEquals(retorno, retornoE);
 	}
-	
+
 	@Test
 	void test6() {
 		ListaEstatica teste2 = new ListaEstatica();
-		
+
 		teste2.inserir(1);
 		teste2.inserir(2);
 		teste2.inserir(3);
@@ -74,20 +74,17 @@ class ListaEstativaTest {
 		teste2.inserir(13);
 		teste2.inserir(14);
 		teste2.inserir(15);
-		
-		
+
 		String retornoE = "1,2,3,4,5,6,7,8,9,10,11,12,13,14,15";
-		
 		String retorno = teste2.toString();
 		assertEquals(retorno, retornoE);
 	}
-	
+
 	@Test
 	void test7() {
 
-		
-		int retornoE = 20 ;
-		
+		int retornoE = 20;
+
 		int retorno = teste.obterElemento(3);
 		assertEquals(retorno, retornoE);
 	}
@@ -95,19 +92,16 @@ class ListaEstativaTest {
 	@Test
 	void test8() {
 
-		Exception exception = assertThrows(RuntimeException.class, () ->
-        teste.obterElemento(5));
-		
+		Exception exception = assertThrows(RuntimeException.class, () -> teste.obterElemento(5));
+
 		assertEquals("Índice 5 fora dos limites para comprimento 4", exception.getMessage());
 	}
-
 
 	@Test
 	void test9() {
 
 		teste.liberar();
-		
-		assertEquals(teste.estaVazia(),true);
-	}
 
+		assertEquals(teste.estaVazia(), true);
+	}
 }
