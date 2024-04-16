70299a1c73c70ec3c9dac596eb75cbdeb834ec1f
Micael Luan Conti <mlconti@furb.br>
2024-04-16 18:36:12 -0300

    fila

--- src/exercicio6/FilaLista.java
+++ src/exercicio6/FilaLista.java
@@ -10,29 +10,24 @@ public class FilaLista<T> implements Fila<T> {
         lista = new ListaEncadeada<>();
     }
 
-    @Override
     public void inserir(T valor) {
         lista.inserirNoFinal(valor);
     }
 
-    @Override
     public boolean estaVazia() {
         return lista.estaVazia();
     }
 
-    @Override
     public T peek() {
         return lista.getPrimeiro().getInfo();
     }
 
-    @Override
     public T retirar() {
         T valor = peek();
         lista.retirar(valor);
         return valor;
     }
 
-    @Override
     public void liberar() {
     }
 
