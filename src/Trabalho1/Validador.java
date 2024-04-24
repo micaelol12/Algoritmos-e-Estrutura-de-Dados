73218d2796f2f0f23d6a94139c8c6693d6477495
Micael Luan Conti <mlconti@furb.br>
2024-04-23 21:38:44 -0300

    feat: start validador

---
+++ src/Trabalho1/Validador.java
@@ -0,0 +1,25 @@
+package Trabalho1;
+
+
+
+public class Validador {
+
+    String teste = "<html>\r\n" + //
+                "<body>\r\n" + //
+                "<h1>Aqui cabeçalho do arquivo</h1>\r\n" + //
+                "<p>Meu parágrafo da página web.</p>\r\n" + //
+                "<p>Meu segundo parágrafo.</p>\r\n" + //
+                "</body>\r\n" + //
+                "</html>";
+
+
+
+
+
+
+
+
+    static void main() {
+        
+    }
+}
