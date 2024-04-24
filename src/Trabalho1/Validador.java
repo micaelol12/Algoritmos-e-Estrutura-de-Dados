1525d7ed6dbbabcadf949a798cf075dadfdb8d8f
MicaelConti <micael.conti@gmail.com>
2024-04-24 19:15:07 -0300

    feat: start validator

--- src/Trabalho1/Validador.java
+++ src/Trabalho1/Validador.java
@@ -1,12 +1,12 @@
 package Trabalho1;
 
 import exercicio5.Pilha;
+import exercicio5.PilhaLista;
 
 public class Validador {
 
-    Pilha tags;
-
-    String teste = "<html>\r\n" + //
+    public static void main(String[] args) {
+        String teste = "<html style={{backgroundColor: 'green'}}>\r\n" + //
                 "<body>\r\n" + //
                 "<h1>Aqui cabeçalho do arquivo</h1>\r\n" + //
                 "<p>Meu parágrafo da página web.</p>\r\n" + //
@@ -14,22 +14,39 @@ public class Validador {
                 "</body>\r\n" + //
                 "</html>";
 
-
-    static void main() {
-
+        validar(teste);
     }
 
+    public static Boolean validar(String texto) {
+        PilhaLista<String> tags = new PilhaLista<String>();
 
-    Boolean validar(String texto) {
+        char[] arrayChar = texto.toCharArray();
 
-        char[] arrayChar =  texto.toCharArray();
+        String tag = "";
 
-        for(int i =0;i <= arrayChar.length - 1;i++){
+        boolean ler = false;
 
-            
+        for (int i = 0; i <= arrayChar.length - 1; i++) {
+
+            char ch = arrayChar[i];
+
+            if (ch == '<') {
+                ler = true;
+            }
+
+            if(ler){
+                tag += ch;
+            }
+
+            if (ch == '>') {
+                tags.push(tag);
+                tag = "";
+                ler = false;
+            }
+
         }
-     
 
+       System.out.println( tags.toString());
 
         return false;
     }
