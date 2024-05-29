14fb76a7205d9262dc5a45d0a9bdae6138213023
Micael Luan Conti <mlconti@furb.br>
2024-05-28 21:38:10 -0300

    feat: start mergesort

---
+++ src/exerciocio10/OrdenacaoMergeSort.java
@@ -0,0 +1,72 @@
+package exerciocio10;
+
+@SuppressWarnings("unchecked")
+public class OrdenacaoMergeSort<T extends Comparable<T>> extends OrdenacaoAbstract<T> {
+
+    public void ordenar() {
+        int n = getInfo().length - 1;
+        
+        mergeSort(0, n);
+    }
+
+    private void mergeSort(int inicio, int fim) {
+        if (inicio < fim) {
+            int meio = (inicio + fim) / 2;
+            mergeSort(inicio, meio);
+            mergeSort(meio + 1, fim);
+            merge(inicio, fim, meio);
+        }
+    }
+
+    private void merge(int inicio, int fim, int meio) {
+        T[] info = getInfo();
+
+        int tamEsquerda = meio - inicio + 1;
+
+        T[] esquerda = (T[]) new Comparable[tamEsquerda];
+
+        for (int i = 0; i <= tamEsquerda - 1; i++) {
+            esquerda[i] = info[inicio + i];
+        }
+
+        int tamDireita = fim - meio;
+
+        T[] direita = (T[]) new Comparable[tamDireita];
+
+        for (int i = 0; i <= tamDireita - 1; i++) {
+            direita[i] = info[meio + 1 + i];
+        }
+
+        int cEsq = 0;
+        int cDir = 0;
+        int i;
+
+        for (i = inicio; i <= fim; i++) {
+
+            if ((cEsq < tamEsquerda) && (cDir < tamDireita)) {
+                if (esquerda[cEsq].compareTo(direita[cDir]) < 0) {
+                    info[i] = esquerda[cEsq];
+                    cEsq++;
+                } else {
+                    info[i] = direita[cDir];
+                    cDir++;
+                }
+            } else {
+                break;
+            }
+
+        }
+
+        while (cEsq < tamEsquerda) {
+            info[i] = esquerda[cEsq];
+            cEsq++;
+            i++;
+        }
+
+        while (cDir < tamDireita) {
+            info[i] = direita[cEsq];
+            cDir++;
+            i++;
+        }
+    }
+}
--- src/exerciocio10/OrdenacaoTest.java
+++ src/exerciocio10/OrdenacaoTest.java
@@ -1,5 +1,29 @@
 package exerciocio10;
 
-public class OrdenacaoTest {
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class OrdenacaoTest {
+    Integer[] numeros;
+    Integer[] numerosOrdenados;
+
+    @BeforeEach
+    public void setUp() {
+        numeros = new Integer[] { 70, 2, 88, 15, 90, 30 };
+        numerosOrdenados = new Integer[] { 2, 15, 30, 70, 88, 90 };
+    }
+
+    @Test
+    void teste4() {
+        OrdenacaoMergeSort<Integer> ordenacao = new OrdenacaoMergeSort<>();
+
+        ordenacao.setInfo(numeros);
+
+        ordenacao.ordenar();
+
+        assertArrayEquals(numerosOrdenados, numeros);
+
+    }
 }
